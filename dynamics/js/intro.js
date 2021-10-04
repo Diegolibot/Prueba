6af0f243b4e22f51d4bfc62a44ef9bbf488d2130
Diegolibot <49957871+Diegolibot@users.noreply.github.com>
2021-10-04 00:57:48 -0500

    Add files via upload

--- dynamics/js/intro.js
+++ dynamics/js/intro.js
@@ -11,7 +11,7 @@ let suitcolor = sessionStorage.getItem("Suit");
     ["dialog","We appreciate your participation as captain of the EROS 5\n mission to Mars with the purpose of advancing to phase II of colonization.",0,1],
     ["dialog"," We know that this great mission requires someone strong, intelligent \nand skilled like you, capable of carrying out the mission successfully.",2,1],
     ["dialog","But before going into any more specifics, please choose your suit.",2,1],
-    ["end","","./menutraje.html"]
+    ["end","","./menutraje.html",1,"chapter1"]
   ];
   
   let imagesrcs = ["JJfeliz.png","back1.png","JJserio.png"]; //las imágenes que se usarán
